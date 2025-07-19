9c246c41a9ab52e03cd22df404351a45e990cd31
Muhammad Gulzar Yahya <gulzaryahya@gmail.com>
2025-07-19 16:03:34 +0500

    Added media routes

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { addMedia } from "../controllers/mediaController.js";
+import { getAllMedia } from "../controllers/mediaController.js";
+import { getMediaById } from "../controllers/mediaController.js";
 
 const router = express.Router();
 
 // POST /api/media - Add new media item
 router.post("/add", addMedia);
+
+// GET /api/media - Get all media items
+router.get("/", getAllMedia);
+
+// GET /api/media/:id - Get media item by ID
+router.get("/:id", getMediaById);
 
 export default router;
